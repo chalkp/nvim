a355646a7bd055d7c0062acc339a0a89b7b98cc9
Sorrawit Poomseetong <buzology@protonmail.com>
2023-12-10 23:52:14 +0700

    remaps and settings

--- lua/chalknvim/remap.lua
+++ lua/chalknvim/remap.lua
@@ -1,1 +1,20 @@
+vim.g.mapleader = " "
 
+vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+Y")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- lua/chalknvim/set.lua
+++ lua/chalknvim/set.lua
@@ -1,24 +1,31 @@
 vim.opt.guicursor = { 'a:ver25' }
 
-vim.opt.wrap = false
+vim.opt.updatetime = 50
 
+-- indents
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
-vim.opt.updatetime = 50
+-- fuck line wrapping
+vim.opt.wrap = false
 
+-- lines
 vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 8
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.opt.isfname:append("@-@")
 
+-- undotree hax
 vim.opt.backup = false
 vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
 
+-- incremental search
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
